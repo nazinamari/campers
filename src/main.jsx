db6738dd479f21f1810d24d4798ebfbaded79af4
Marina Nazina <nzyame@gmail.com>
2024-08-11 19:26:51 +0100

    styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App.jsx';
 import './index.css';
-import 'modern-normalize';
 import { store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
