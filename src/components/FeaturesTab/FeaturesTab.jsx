494d37b117f51f047de965c9868774de5083fa59
Marina Nazina <nzyame@gmail.com>
2024-09-07 13:51:42 +0100

    vehicle details

--- src/components/FeaturesTab/FeaturesTab.jsx
+++ src/components/FeaturesTab/FeaturesTab.jsx
@@ -1,12 +1,18 @@
 import BookingForm from '../BookingForm /BookingForm';
 import Features from '../Features/Features';
+import VehicleDetails from '../VehicleDetails/VehicleDetails';
 import css from './FeaturesTab.module.css';
 
 export default function FeaturesTab({ data }) {
 	return (
 		<div className={css.featuresTab_container}>
-			<Features data={data} />
-			<BookingForm />
+			<div className={css.left}>
+				<Features data={data} />
+				<VehicleDetails data={data} />
+			</div>
+			<div className={css.right}>
+				<BookingForm />
+			</div>
 		</div>
 	);
 }
