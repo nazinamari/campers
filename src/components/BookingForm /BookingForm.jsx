d674a196575b1c3505e2d43fdb3acac5c652464b
Marina Nazina <nzyame@gmail.com>
2024-09-08 12:47:18 +0100

    form

--- src/components/BookingForm /BookingForm.jsx
+++ src/components/BookingForm /BookingForm.jsx
@@ -1,7 +1,14 @@
+import Button from '../../shared/components/Button/Button';
+import css from './BookingForm.module.css';
+
 export default function BookingForm() {
 	return (
 		<>
-			<p>Form</p>
+			<h3 className={css.title}>Book your campervan now</h3>
+			<p className={css.description}>
+				Stay connected! We are always ready to help you.
+			</p>
+			<Button className={css.btnSend}>Send</Button>
 		</>
 	);
 }
