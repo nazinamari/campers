7f704054bed11ce25d306b9e8340835824aa2065
Marina Nazina <nzyame@gmail.com>
2024-09-13 17:38:08 +0100

    custom btns

--- src/shared/components/Slider/Slider.jsx
+++ src/shared/components/Slider/Slider.jsx
@@ -2,8 +2,15 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import css from './Slider.module.css';
+import Icon from '../Icon/Icon';
+import { useEffect } from 'react';
 
 export default function Slider({ data, component: Component }) {
+	useEffect(() => {
+		const swiper = document.querySelector('.swiper').swiper;
+		swiper.navigation.update();
+	}, []);
+
 	return (
 		<div className={css.swiper_container}>
 			<Swiper
@@ -21,9 +28,15 @@ export default function Slider({ data, component: Component }) {
 						<Component {...item} />
 					</SwiperSlide>
 				))}
-				<div className={`${css.swiper_button_prev} swiper-button-prev`}></div>
-				<div className={`${css.swiper_button_next} swiper-button-next`}></div>
 			</Swiper>
+			<div className={css.swiper_buttons_container}>
+				<div className={`${css.swiper_button_prev} swiper-button-prev`}>
+					<Icon className={css.icon} id="i-left" width="100" height="100" />
+				</div>
+				<div className={`${css.swiper_button_next} swiper-button-next`}>
+					<Icon className={css.icon} id="i-right" width="100" height="100" />
+				</div>
+			</div>
 		</div>
 	);
 }
