8d471f85b384e256afc8536575eea74a703abf1b
Marina Nazina <nzyame@gmail.com>
2024-09-13 17:19:05 +0100

    custom swiper

--- src/shared/components/Slider/Slider.jsx
+++ src/shared/components/Slider/Slider.jsx
@@ -1,26 +1,29 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
 import css from './Slider.module.css';
 
 export default function Slider({ data, component: Component }) {
 	return (
-		<Swiper
-			grabCursor={true}
-			slidesPerView={1}
-			modules={[Navigation]}
-			navigation={{
-				prevEl: `.${css.swiperButtonPrev}`,
-				nextEl: `.${css.swiperButtonNext}`,
-			}}
-			className={css.swiperContainer}
-		>
-			{data.map((item) => (
-				<SwiperSlide key={item.id}>
-					<Component {...item} />
-				</SwiperSlide>
-			))}
-		</Swiper>
+		<div className={css.swiper_container}>
+			<Swiper
+				grabCursor={true}
+				slidesPerView={1}
+				spaceBetween={30}
+				modules={[Navigation]}
+				navigation={{
+					nextEl: `.${css.swiper_button_next}`, // Кастомний клас для "далі"
+					prevEl: `.${css.swiper_button_prev}`, // Кастомний клас для "назад"
+				}}
+			>
+				{data.map((item) => (
+					<SwiperSlide key={item.id} className={css.wrapper}>
+						<Component {...item} />
+					</SwiperSlide>
+				))}
+				<div className={`${css.swiper_button_prev} swiper-button-prev`}></div>
+				<div className={`${css.swiper_button_next} swiper-button-next`}></div>
+			</Swiper>
+		</div>
 	);
 }
