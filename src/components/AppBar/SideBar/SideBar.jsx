a82da6363adbab18c975faf1d31c322ec8be5c95
Marina Nazina <nzyame@gmail.com>
2024-09-24 19:30:28 +0100

    sidebar

--- src/components/AppBar/SideBar/SideBar.jsx
+++ src/components/AppBar/SideBar/SideBar.jsx
@@ -1,12 +1,29 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect } from 'react';
 import css from './SideBar.module.css';
 import Filters from '../../Filters/Filters';
 import Logo from '../../../shared/components/Logo/Logo';
 
 export default forwardRef(function Sidebar({ isOpen, close }, ref) {
+	// Використовуємо useEffect для блокування прокрутки при відкритті сайдбару
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden'; // Блокуємо прокрутку сторінки
+		} else {
+			document.body.style.overflow = ''; // Відновлюємо прокрутку
+		}
+
+		// Повертаємо функцію очищення, щоб скинути прокрутку при розмонтуванні
+		return () => {
+			document.body.style.overflow = ''; // Скидаємо стиль overflow при закритті або розмонтуванні
+		};
+	}, [isOpen]);
+
 	return (
 		<div ref={ref} className={`${css.sidebar} ${isOpen ? css.open : ''}`}>
+			{/* Рендеримо фільтри тільки якщо сайдбар відкритий */}
 			{isOpen && <Filters close={close} />}
+
+			{/* Логотип */}
 			<div className={css.logo}>
 				<Logo />
 			</div>
